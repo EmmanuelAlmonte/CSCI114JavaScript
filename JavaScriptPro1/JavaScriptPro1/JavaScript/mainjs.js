004e5a30598878560d03ecad33a59f2b7ca95402
EmmanuelAlmonte <ammetronics67@gmail.com>
2022-01-31 17:08:33 -0500

    First Javascript assignment

---
+++ JavaScriptPro1/JavaScriptPro1/JavaScript/mainjs.js
@@ -0,0 +1,19 @@
+// store first noun
+let firstNoun = prompt('Please enter a noun: ');
+// store first verb
+let firstVerb = prompt('Please enter a verb: ');
+// store second noun
+let secondNoun = prompt('Please Enter a noun: ');
+// store third noun
+let thirdNoun = prompt('Please enter a noun: ');
+// store first number
+let number0 = Number(prompt('Please enter a number: '));
+// store second number
+let number1 = Number(prompt('Please enter a number: '));
+// store third number
+let number2 = Number(prompt('Please enter a number: '));
+// store the addition of two numbers
+let total = number1+number2;
+// Display all data captured in a Mad Lib
+console.log("Hey, my name is " +firstNoun+" and I like to " + firstVerb+"."+" My age is "+number0+"."+" My savings account has "
++number1+" and my checking account balance amount is "+number2+"."+ "The total amount in both accounts is " +total+"."+" I work at "+secondNoun+" and I attend the University of "+thirdNoun+".");
